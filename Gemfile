595a32d437fcb0455932c260df93f6aacb1de6f5
Andrew Fomera <andrew@zerlex.net>
2012-07-14 00:18:50 -0500

    Add Foundation/Site Layout

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
+source 'https://rubygems.org'
+
 gem 'sinatra', :require => 'sinatra/base'
 gem 'zurb-foundation', '~>3.0'
+gem 'pony'
--- andrew.rb
+++ andrew.rb
@@ -1,16 +1,38 @@
 class AndrewApp < Sinatra::Base
   get '/' do
-     @title = 'Welcome!'
-      erb :index, :layout => :layour2
+     @title = 'Welcome'
+      erb :index
   end
 
   get '/about' do
+    @title = 'About Me'
+
     erb :about
   end
 
   get '/contact' do
+    @title = 'Contact Me'
     erb :contact
   end
+
+  post '/contact' do
+    require 'pony'
+    @contact_name = params['contact-name']
+    @contact_email = params['contact-email']
+    @contact_message = params['contact-message']
+
+    @my_email = 'andrew.fomera@gmail.com'
+    @email_subject = 'Hey! You have an email from your site!'
+
+    Pony.mail :to => @my_email,
+              :from => @contact_email,
+              :subject => @email_subject,
+              :body => @contact_message
+
+    @contact_name.to_s
+
+  end
+
 
 end
 
